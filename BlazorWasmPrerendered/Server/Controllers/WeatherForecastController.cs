7651c57f457f08a6aff29846108492d954a3dbbd
Ron Zhong <ron.zhong@helixleisure.com>
2021-12-29 08:30:09 +0800

    + net6 demos #1

---
+++ BlazorWasmPrerendered/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using BlazorWasmPrerendered.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmPrerendered.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = 25, //Random.Shared.Next(-20, 55),
+                Summary = Summaries[0]//Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
