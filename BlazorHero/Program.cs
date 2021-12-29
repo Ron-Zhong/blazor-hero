077d69e06363a563a9b3b6645235dd5d1d0d433a
Ron Zhong <ron.zhong@helixleisure.com>
2021-12-29 09:13:50 +0800

    + net6 demo #2

--- BlazorHero/Program.cs
+++ BlazorHero/Program.cs
@@ -1,6 +1,6 @@
-using BlazorHero;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RonBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
